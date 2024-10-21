1c8dc7e5f16ba6a4983330b138ca8f8d20420092
suki <nuptunia.me@proton.me>
2024-10-21 17:29:48 -0500

    Update VisualOnlyFire.h

--- Examples/VisualOnlyFire.h
+++ Examples/VisualOnlyFire.h
@@ -1,2 +1,2 @@
-int StopPacket = 0x4E800020;
+int StopPacket = 0x4E800020; // by nuptunia
 void VisualOnlyFire(bool toggle) { if (toggle) { *(int*)0x0237BD0 = StopPacket; } else { *(int*)0x0237BD0 = 0xF821FF91; } }
